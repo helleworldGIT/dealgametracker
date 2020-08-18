3446fe2d96b235e200284786bb566cec89f2929f
Desiré <44606766+helleworldGIT@users.noreply.github.com>
2020-08-18 11:02:01 +0200

    v0.1

--- src/views/Stores.vue
+++ src/views/Stores.vue
@@ -6,7 +6,7 @@
       <p class="info_points">
           🔴 Red points mean the store is <b>not active</b> right now,
           <br>
-          🟢 green points mean they're <b>active</b>.
+          🟩 green squares mean they're <b>active</b>.
       </p>
       <ul>
           <li v-for="store in stores" :key="store.id">
@@ -15,7 +15,7 @@
                   🔴
               </p>
               <p :class="{hide: store.isActive === 0, show: store.isActive === 1}">
-                  🟢
+                  🟩
               </p>
                   {{store.storeName}}
               </h2>
